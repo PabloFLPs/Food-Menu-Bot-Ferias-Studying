983dbe834667b1275516d80b3ab146bbd7a8a0bf
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-08-29 16:18:41 -0300

    Treating 'undefined' data and possible wrong readings

--- src/index.js
+++ src/index.js
@@ -33,41 +33,53 @@ const parseExcel = (fileName) => {
 // This is the offset to get our current day in the .xls file:
 let currentXlsDay = currentDay - 3
 
-parseExcel("./files/Cardápio-AGOSTO.xlsx").forEach(element => {
+parseExcel(`./files/Cardápio-${currentMonth}.xlsx`).forEach(element => {
+    let day = element.data[currentXlsDay][`${currentMonth}`]
+    let dayName = element.data[currentXlsDay]["__EMPTY"]
+    let month = `${currentMonth}`[0] + `${currentMonth}`.toLowerCase().slice(1)
+
+    if (!element.data[currentXlsDay]["__EMPTY"]){
+        day = element.data[currentXlsDay][`${currentMonth}`][0] // setting correct day by treating the data
+        dayName = element.data[currentXlsDay][`${currentMonth}`].slice(1).replace(/ /g, "") // setting correct dayName by treating the data
+    }
+
+    // Here we verify if the last item from the menu is a valid item; if it is, we continue to show the menu:
+    if ((element.data[currentXlsDay]["__EMPTY_11"])){
+        console.log(
+            "Data: " + dayName + ", " + day + " de " + month
+        )
     
-    console.log(
-        "Data: " +
-        element.data[currentXlsDay]["__EMPTY"] + // day name
-        ", " +
-        element.data[currentXlsDay][`${currentMonth}`] + // day
-        " de " +
-        `${currentMonth}`[0] + `${currentMonth}`.toLowerCase().slice(1) // month
-    )
-
-    // Remove acentuation:
-    // exampleString.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-
-    console.log(
-        "- CARDÁPIO - " +
-        "\n" +
-        "Principal: " + element.data[currentXlsDay]["__EMPTY_1"] + " e " + element.data[currentXlsDay]["__EMPTY_2"] +
-        "\n" +
-        "Prato Protéico: " + element.data[currentXlsDay]["__EMPTY_3"] +
-        "\n" +
-        "Vegetariana: " + element.data[currentXlsDay]["__EMPTY_4"] +
-        "\n" +
-        "Vegana: " + element.data[currentXlsDay]["__EMPTY_5"] +
-        "\n" +
-        "Guarnição: " + element.data[currentXlsDay]["__EMPTY_6"] + 
-        "\n" +
-        "Salada - Folhas: " + element.data[currentXlsDay]["__EMPTY_7"] +
-        "\n" +
-        "Salada - Legumes/Acompanhamentos: " + element.data[currentXlsDay]["__EMPTY_8"] +
-        "\n" +
-        "Salada - Cozidos: " + element.data[currentXlsDay]["__EMPTY_9"] +
-        "\n" +
-        "Salada - Composta: " + element.data[currentXlsDay]["__EMPTY_10"] +
-        "\n" +
-        "Sobremesa: " + element.data[currentXlsDay]["__EMPTY_11"]
-    )
+        // Remove accentuation:
+        // exampleString.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+        
+        let weekDay = new Date().getDay() // getting current day of the week
+        if (weekDay == 0 || weekDay == 6) { // if it's weekend, there is no menu
+            console.log("Não há cardápio hoje :(")
+        }
+        else {
+            console.log(
+                "- CARDÁPIO - " +
+                "\n" +
+                "Principal: " + element.data[currentXlsDay]["__EMPTY_1"] + " e " + element.data[currentXlsDay]["__EMPTY_2"] +
+                "\n" +
+                "Prato Protéico: " + element.data[currentXlsDay]["__EMPTY_3"] +
+                "\n" +
+                "Vegetariana: " + element.data[currentXlsDay]["__EMPTY_4"] +
+                "\n" +
+                "Vegana: " + element.data[currentXlsDay]["__EMPTY_5"] +
+                "\n" +
+                "Guarnição: " + element.data[currentXlsDay]["__EMPTY_6"] + 
+                "\n" +
+                "Salada - Folhas: " + element.data[currentXlsDay]["__EMPTY_7"] +
+                "\n" +
+                "Salada - Legumes/Acompanhamentos: " + element.data[currentXlsDay]["__EMPTY_8"] +
+                "\n" +
+                "Salada - Cozidos: " + element.data[currentXlsDay]["__EMPTY_9"] +
+                "\n" +
+                "Salada - Composta: " + element.data[currentXlsDay]["__EMPTY_10"] +
+                "\n" +
+                "Sobremesa: " + element.data[currentXlsDay]["__EMPTY_11"]
+            )
+        }
+    }
 })
